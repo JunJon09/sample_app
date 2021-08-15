8cd0d3a643d141e6b63da374ae33ec76672ce9cc
JunJon09 <shibajunjun14@gmail.com>
2021-08-15 04:52:56 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,12 +8,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
    test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     #assert_selectは特定の中に存在するかの有無を確認
     #今回ならtitleにHome | Ruby on Rails Tutorial Sample Appがあるか
     assert_response :success
@@ -21,19 +21,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
   
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
